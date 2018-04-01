dfd3d396b2e98b08503f5efd7c10d41a23052574
Malu <malu@example.com>
2018-04-01 10:28:44 +0200

    Letsencrypt

--- bin/host-startup.sh
+++ bin/host-startup.sh
@@ -3,5 +3,5 @@
 cd /var/www/reverse-proxy && docker-compose up
 
 cd /var/www/gogo && make prod-quick
-#cd /var/www/evett && make prod
+cd /var/www/evett && make prod
 #cd /var/www/kalli && make prod
