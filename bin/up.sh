659f7a44f55b3571f9c7de8329f8ac0589c417d1
Malu <malu@example.com>
2018-04-23 08:31:20 +0200

    Add env variable check

--- bin/up.sh
+++ bin/up.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -z "$NGINX_COOKIE_PW" ]; then
+    echo "Need to set NGINX_COOKIE_PW"
+    exit 1
+fi
+
 sed -i "s/NGINX_COOKIE_PW/$NGINX_COOKIE_PW/g" my_default.conf
 sed -i "s/NGINX_COOKIE_PW/$NGINX_COOKIE_PW/g" vhost.d/default
