7762181b846746cc552fb4f4aaeb602ac76a82fe
vorsichtique <malu3400@gmail.com>
2018-11-04 13:27:26 +0100

    Modify password check

--- bin/up.sh
+++ bin/up.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-if [ -z "$NGINX_COOKIE_PW" ]; then
-    echo "Need to set NGINX_COOKIE_PW"
+# make sure niginx cookie password was set:
+
+if grep -Fxq "$NGINX_COOKIE_PW" my_default.conf
+then
     exit 1
 fi
 
-sed -i "s/NGINX_COOKIE_PW/$NGINX_COOKIE_PW/g" my_default.conf
-sed -i "s/NGINX_COOKIE_PW/$NGINX_COOKIE_PW/g" vhost.d/default
+if grep -Fxq "$NGINX_COOKIE_PW" vhost.d/default
+then
+    exit 1
+fi
+
+exit 0
